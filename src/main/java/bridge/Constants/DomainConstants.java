006d18f0a30e960a33d067d26b5030241d56b11e
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-11-25 13:18:23 +0900

    feat: 도메인 로직과 연관된 상수 추가

---
+++ src/main/java/bridge/Constants/DomainConstants.java
@@ -0,0 +1,10 @@
+package bridge.Constants;
+
+public class DomainConstants {
+    public static final Integer MIN_BRIDGE_SIZE = 3;
+    public static final Integer MAX_BRIDGE_SIZE = 20;
+    public static final String MOVE_DIRECTION_UP = "U";
+    public static final String MOVE_DIRECTION_DOWN = "D";
+    public static final String GAME_COMMAND_RETRY = "R";
+    public static final String GAME_COMMAND_QUIT = "Q";
+}
